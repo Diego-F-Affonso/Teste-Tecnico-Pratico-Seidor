020c75aef4a17a4e8b941ca2f52e092edd4ae532
Diego Fernandes Affonso <diegofernandesaffonso@gmail.com>
2021-02-18 16:22:37 -0300

    Css tableInformation

--- src/components/tableInformation/index.js
+++ src/components/tableInformation/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './index.css';
 
 class TableInformation extends Component {
   render() {
     return (
-      <div>
+      <div className="information__wrapper">
         <h1>Tabelas e cálculos do IRRF</h1>
         <p>A tabela do IR é um dos principais instrumentos para auxiliar os contribuintes na hora de enviar as informações fiscais para a Receita. Afinal, é nesse documento que constam as alíquotas do Imposto de Renda</p>
         <p>Isso quer dizer que é essa a fonte para você saber qual é o percentual que deve ser aplicado sobre os seus rendimentos.<br />Portanto, na hora de fazer cálculo e declarar seus rendimentos, ter essa tabela é fundamental para que você não envie <b>nenhum dado errado e, consequentemente, não caia na malha fina.</b></p>
