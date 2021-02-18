58a89ec214d12bd1244525df7c1ab259bece42ee
Diego Fernandes Affonso <diegofernandesaffonso@gmail.com>
2021-02-18 18:05:58 -0300

    Revisao/Refatoraçao Tela de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from '../../images/logoSeidor.png';
 import './index.css';
 
 class Login extends Component {
@@ -6,8 +7,12 @@ class Login extends Component {
     super(props);
     this.state = {
       email: '',
+      password: '',
+      isDisable: true,
     }
 
+    this.validateEmail = this.validateEmail.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -16,11 +21,38 @@ class Login extends Component {
     history.push('registrar-funcionario')
   }
 
+  // setState
+  handleInputChange({ name, value }) {
+    this.setState({ [name]: value }, () => {
+      const { email } = this.state;
+      if (email !== '') {
+        this.setState({ isDisable: false });
+      } else {
+        this.setState({ isDisable: true });
+      }
+    });
+  }
+
+   // https://www.itsolutionstuff.com/post/react-email-validation-exampleexample.html  valida email e tamanho min senha
+   validateEmail() {
+    const { email, password } = this.state;
+    const regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    const resultRegex = regex.test(email);
+    const minValue = 6;
+
+    if (resultRegex && password.length >= minValue) {
+      return this.setState({ isDisable: false });
+    } else{
+      this.setState({ isDisable: true });
+    }
+  }
+
   render() {
+    const { email, isDisable } = this.state;
     return (
       <main className="login__wrapper">
         <form className="login__container">
-          <img src="https://www.seidor.com.br/etc/designs/seidorweb/images/logoSeidor.png" alt="seidor logo"/>
+          <img src={ logo } alt="seidor logo"/>
           <label htmlFor="email">
             Email:
             <input
@@ -28,19 +60,25 @@ class Login extends Component {
               name="email"
               type="text"
               placeholder="seidor@seidor.com"
+              value={ email }
+              onKeyUp={ this.validateEmail }
+              onChange={ ({ target }) => this.handleInputChange(target) }
             />
           </label>
-          <label htmlFor="senha">
+          <label htmlFor="password">
             Senha:
             <input
-              id="senha"
-              name="senha"
+              id="password"
+              name="password"
               type="password"
               placeholder="*******"
+              onKeyUp={ this.validateEmail }
+              onChange={ ({ target }) => this.handleInputChange(target) }
             />
           </label>
           <button
             type="button"
+            disabled={ isDisable }
             onClick={ () => this.handleSubmit() }
           >
             LOGIN
